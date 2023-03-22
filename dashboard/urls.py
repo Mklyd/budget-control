92ed2b7631643776d67d9950137b703cc56e0b1e
Mklyd <mastklydm@gmail.com>
2023-03-22 08:44:42 +0300

    first commit

---
+++ dashboard/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from dashboard.views import HomePageView, CategoryAddView, PaymentAddView
+
+urlpatterns = [
+    path('', HomePageView.as_view(), name='home'),
+    path('category_add/', CategoryAddView.as_view(), name='category_add'),
+    path('payment_add/<int:pk>/', PaymentAddView.as_view(), name='payment_add')
+
+]
